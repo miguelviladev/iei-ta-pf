4ce1dc490d7b66343d5e954afa6c5fa4b5326be8
Miguel Vila <miguel.vila.dev@gmail.com>
2022-02-01 16:58:28 +0000

    Improved navbbar navigation on small devices

--- projeto/js/navbar.js
+++ projeto/js/navbar.js
@@ -1,4 +1,8 @@
-function esconder() {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+async function esconder() {
   const menuToggle = document.getElementById("navbarNav");
   const bsCollapse = new bootstrap.Collapse(menuToggle);
   bsCollapse.toggle();
